4474c3a3d812b9fe7888c5399c4a522b8629a021
idranme <96647698+idranme@users.noreply.github.com>
2026-07-30 15:36:44 +0800

    fix(onebot): add types & use objectToSnake
    Introduce a DeepPartial AlbumInfo type and update GetGroupAlbumList to return AlbumInfo[]. Replace verbose manual mapping with objectToSnake(a) and explicitly stringify create/modify/last_upload/upload_number fields. Add imports for DeepPartial and objectToSnake and tighten BaseAction generic. Behavior and error handling remain the same; mapping is now simpler and preserves snake_case output.

--- src/onebot11/action/llbot/group/GroupAlbum/GetGroupAlbumList.ts
+++ src/onebot11/action/llbot/group/GroupAlbum/GetGroupAlbumList.ts
@@ -1,11 +1,98 @@
+import { DeepPartial } from '@/common/utils'
 import { BaseAction, Schema } from '../../../BaseAction'
 import { ActionName } from '../../../types'
+import { objectToSnake } from 'ts-case-convert'
 
 interface Payload {
   group_id: number | string
 }
 
-export class GetGroupAlbumList extends BaseAction<Payload, unknown> {
+type AlbumInfo = DeepPartial<{
+  album_id: string
+  owner: string
+  name: string
+  desc: string
+  create_time: string
+  modify_time: string
+  last_upload_time: string
+  upload_number: string
+  cover: {
+    type: number
+    image: {
+      name: string
+      sloc: string
+      lloc: string
+      photo_url: {
+        spec: number
+        url: {
+          url: string
+          width: number
+          height: number
+        }
+      }[]
+      default_url: {
+        url: string
+        width: number
+        height: number
+      }
+      is_gif: boolean
+      has_raw: boolean
+    }
+    video: unknown
+    desc: string
+    lbs: unknown
+    uploader: string
+    batch_id: string
+    upload_time: string
+    upload_order: number
+    like: unknown
+    comment: unknown
+    upload_user: unknown
+    ext: unknown[]
+    shoot_time: string
+    link_id: string
+    op_mask: unknown[]
+    lbs_source: number
+  }
+  creator: {
+    uid: string
+    nick: string
+    yellow_info: unknown
+    star_info: unknown
+    is_sweet: boolean
+    is_special: boolean
+    is_super_like: boolean
+    custom_id: string
+    poly_id: string
+    portrait: string
+    can_follow: number
+    isfollowed: number
+    uin: string
+    ditto_uin: string
+  }
+  top_flag: string
+  busi_type: number
+  status: number
+  permission: null | string
+  allow_share: boolean
+  is_subscribe: boolean
+  bitmap: string
+  is_share_album: boolean
+  share_album: null | string
+  qz_album_type: number
+  family_album: null | string
+  lover_album: null | string
+  cover_type: number
+  travel_album: null | string
+  visitor_info: null | string
+  default_desc: string
+  op_info: null | string
+  active_album: null | string
+  memory_info: null | string
+  sort_type: number
+}>
+
+export class GetGroupAlbumList extends BaseAction<Payload, AlbumInfo[]> {
   actionName = ActionName.GetGroupAlbumList
   payloadSchema = Schema.object({
     group_id: Schema.union([Number, String]).required()
@@ -16,43 +103,12 @@ export class GetGroupAlbumList extends BaseAction<Payload, unknown> {
     if (result.retCode !== 0) {
       throw new Error(result.retMsg)
     }
-    return result.albumList.map((a) => {
-      const photoUrls = a.cover?.image?.photoUrls ?? []
-      const defaultUrl = a.cover?.image?.defaultUrl
-      return {
-        album_id: a.albumId,
-        owner: a.owner,
-        name: a.name,
-        desc: a.desc,
-        create_time: String(a.createTime),
-        modify_time: String(a.modifyTime),
-        last_upload_time: String(a.lastUploadTime),
-        upload_number: String(a.uploadNumber),
-        cover: {
-          type: a.cover?.type ?? 0,
-          image: a.cover?.image ? {
-            lloc: a.cover.image.lloc,
-            photo_url: photoUrls.map((p) => ({
-              spec: p.spec,
-              url: {
-                url: p.url.url,
-                width: p.url.width,
-                height: p.url.height
-              },
-            })),
-            default_url: defaultUrl ? {
-              url: defaultUrl.url,
-              width: defaultUrl.width,
-              height: defaultUrl.height
-            } : null,
-          } : null,
-          desc: a.desc,
-        },
-        creator: {
-          nick: a.creator?.nick ?? '',
-          uin: a.creator?.uin ?? '',
-        },
-      }
-    })
+    return result.albumList.map(a => ({
+      ...objectToSnake(a),
+      create_time: a.createTime.toString(),
+      modify_time: a.modifyTime.toString(),
+      last_upload_time: a.lastUploadTime.toString(),
+      upload_number: a.uploadNumber.toString()
+    }))
   }
 }
